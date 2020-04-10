d29bbb352b359ee300d7596874de13076ef22fd3
AsA Sistemas de Computaçao <asasistemas@MacBook-Pro-de-AsA.local>
2020-04-09 21:42:27 -0300

    New addition of README files and change in index.html

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1 align="center">
+<br>
+  <img src="arquivos/logo.jpg" alt="Memorygame" width="90">
+<br>
+<br>
+  Memory Game
+</h1>
+
+<h4 align="center">
+  The memory game is a classic game made up of pieces that feature a figure on one side. Each figure is repeated in two different pieces. To start the game, the pieces are placed with the figures facing downwards, so that they cannot be seen. Each participant must, in turn, turn two pieces and let everyone see them. If the figures are the same, the participant must collect this pair with him and play again. If they are different pieces, they must be turned over again, and the next participant must pass the turn. Whoever has the most pairs at the end of the game wins the game.
+</h4>
+<p align="center">
+  <a href="http://makeapullrequest.com">
+    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square" alt="PRs Welcome">
+  </a>
+  <a href="https://opensource.org/licenses/MIT">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square" alt="License MIT">
+  </a>
+</p>
+
+<p align="center">
+  <a href="#rocket-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#getting-started">Getting started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#contributing">Contributing</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#license">License</a>
+</p>
+
+<div>
+    <img src="arquivos/readme.png" alt="Memorygame">
+</div>
+
+## :rocket: Technologies
+
+-   [Node.js](https://nodejs.org/en/)
+-   [Bootstrap 4](https://getbootstrap.com/docs/4.4/getting-started/introduction/)
+-   [VS Code](https://code.visualstudio.com) with [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+
+## Getting started
+
+1. Clone this repo using `git@github.com:joaoathaide/memory-game.git`
+2. Move to the appropriate directory: `cd memory-game`.<br />
+3. Run `yarn` to install dependencies.<br />
+4. Drag the `index.html` to the `browser`.<br/>
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+Made with ♥ by João Athaide
--- src/jogoDaMemoria.js
+++ src/jogoDaMemoria.js
@@ -1,78 +1,82 @@
-
 class JogoDaMemoria {
-
-
     constructor({ tela, util }) {
-        this.tela = tela
-        this.util = util
+        this.tela = tela;
+        this.util = util;
 
-        this.iconePadrao = './arquivos/default.png'
+        this.iconePadrao = './arquivos/default.png';
         this.heroisIniciais = [
             { img: './arquivos/batman.png', nome: 'batman' },
             { img: './arquivos/ciclop.png', nome: 'ciclop' },
             { img: './arquivos/deadpool.png', nome: 'deadpool' },
             { img: './arquivos/mulhermaravilha.png', nome: 'mulhermaravilha' },
-        ]
+        ];
 
-        this.heroisEscondidos = []
-        this.heroisSelecionados = []
+        this.heroisEscondidos = [];
+        this.heroisSelecionados = [];
     }
 
     inicializar() {
-        this.tela.atualizarImagens(this.heroisIniciais)
-        // quando essa função executar, vai ignorar o this de window 
+        this.tela.atualizarImagens(this.heroisIniciais);
+        // quando essa função executar, vai ignorar o this de window
         // ela vai usar o this dessa tela
-        this.tela.configurarBotaoJogar(this.jogar.bind(this))
-        this.tela.configurarClickVerificarSelecao(this.verificarSelecao.bind(this))
-        this.tela.configurarBotaoMostrarTudo(this.mostrarHeroisEscondidos.bind(this))
-
+        this.tela.configurarBotaoJogar(this.jogar.bind(this));
+        this.tela.configurarClickVerificarSelecao(
+            this.verificarSelecao.bind(this)
+        );
+        this.tela.configurarBotaoMostrarTudo(
+            this.mostrarHeroisEscondidos.bind(this)
+        );
     }
     esconderHerois(herois) {
         const heroisOcultos = herois.map(({ nome, id }) => ({
             id,
             nome,
-            img: this.iconePadrao
-        }))
+            img: this.iconePadrao,
+        }));
 
-        this.tela.atualizarImagens(heroisOcultos)
-        this.heroisEscondidos = heroisOcultos
+        this.tela.atualizarImagens(heroisOcultos);
+        this.heroisEscondidos = heroisOcultos;
     }
 
     exibirHerois(nomeHeroi) {
-        const { img } = this.heroisIniciais.find(({ nome }) => nomeHeroi === nome)
-        this.tela.exibirHerois(nomeHeroi, img)
+        const { img } = this.heroisIniciais.find(
+            ({ nome }) => nomeHeroi === nome
+        );
+        this.tela.exibirHerois(nomeHeroi, img);
     }
 
     verificarSelecao(id, nome) {
-        const item = { id, nome }
+        const item = { id, nome };
         // alert(`Olá: ${nome}, id: ${id}`)
-        const heroisSelecionados = this.heroisSelecionados.length
+        const heroisSelecionados = this.heroisSelecionados.length;
         switch (heroisSelecionados) {
             case 0:
-                this.heroisSelecionados.push(item)
+                this.heroisSelecionados.push(item);
                 break;
             case 1:
-                const [opcao1] = this.heroisSelecionados
+                const [opcao1] = this.heroisSelecionados;
                 // zerar itens, para nao selecionar mais de dois
-                this.heroisSelecionados = []
-                let deveMostrarMensagem = false
+                this.heroisSelecionados = [];
+                let deveMostrarMensagem = false;
                 if (opcao1.nome === item.nome && opcao1.id !== id) {
-                    deveMostrarMensagem = true
-                    this.exibirHerois(item.nome)
-                    this.tela.exibirMensagem(true)
+                    deveMostrarMensagem = true;
+                    this.exibirHerois(item.nome);
+                    this.tela.exibirMensagem(true);
                     return;
                 }
-                this.tela.exibirMensagem(false)
+                this.tela.exibirMensagem(false);
                 break;
         }
     }
     mostrarHeroisEscondidos() {
-        const heroisEscondidos = this.heroisEscondidos
+        const heroisEscondidos = this.heroisEscondidos;
         for (const heroi of heroisEscondidos) {
-            const { img } = this.heroisIniciais.find(item => item.nome === heroi.nome)
-            heroi.img = img
+            const { img } = this.heroisIniciais.find(
+                (item) => item.nome === heroi.nome
+            );
+            heroi.img = img;
         }
-        this.tela.atualizarImagens(heroisEscondidos)
+        this.tela.atualizarImagens(heroisEscondidos);
     }
     async embaralhar() {
         const copias = this.heroisIniciais
@@ -81,25 +85,23 @@ class JogoDaMemoria {
             .concat(this.heroisIniciais)
             // entrar em cada um dos itens e gerar um id para cada
             .map((item) => {
-                return Object.assign({}, item, { id: (Math.random() / 0.5) })
+                return Object.assign({}, item, { id: Math.random() / 0.5 });
             })
             // ordenar
-            .sort(() => Math.random() - 0.5)
+            .sort(() => Math.random() - 0.5);
 
-        this.tela.atualizarImagens(copias)
-        this.tela.exibirCarregando()
+        this.tela.atualizarImagens(copias);
+        this.tela.exibirCarregando();
 
-        const idIntervalo = this.tela.iniciarContador()
+        const idIntervalo = this.tela.iniciarContador();
         await this.util.timeout(3000);
-        this.tela.limparContador(idIntervalo)
+        this.tela.limparContador(idIntervalo);
 
-        this.esconderHerois(copias)
-        this.tela.exibirCarregando(false)
-
+        this.esconderHerois(copias);
+        this.tela.exibirCarregando(false);
     }
 
     jogar() {
-        this.embaralhar()
+        this.embaralhar();
     }
-
 }
